f4a8e8e0d518d531c61a3f677286c724d26ea3c7
Greg Broiles <gbroiles@gmail.com>
2021-11-17 01:44:58 +0000

    remove smtp, change to syslog logging

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 import sys
-import smtplib
 import datetime
+import syslog
 from pynput import keyboard
 from threading import Timer
 
@@ -14,47 +14,19 @@ class Keylogger:
     def handle_key_press(self, key):
         self.count += 1
 
-    def request_mail_credentials(self):
-        try:
-            self.email = os.environ["counter_email"]
-        except KeyError:
-            print("Must set counter_email enviroment variable.")
-            sys.exit(1)
-        try:
-            self.password = os.environ["counter_password"]
-        except KeyError:
-            print("Must set counter_password enviroment variable.")
-            sys.exit(1)
-        print("Got environment variables", flush=True)
-
-    def send_mail(self, email, password, msg):
-        print("Getting ready to send email report.",flush=True)
-        try:
-            server = smtplib.SMTP('smtp.gmail.com', 587)
-            server.ehlo()
-            server.starttls()
-            server.login(email, password)
-            server.sendmail(email, email, msg)
-        except Exception as e:
-            print('An error occurred: ', e)
-        finally:
-            server.quit()
-        print("Email report sent.",flush=True)
-
     def report(self):
-        log_date = datetime.datetime.now()
-        msg = f'Subject: Log info {log_date}\n\nKeystrokes: {self.count}\n'
-        self.send_mail(self.email, self.password, msg)
+        msg = f'Keystrokes: {self.count}'
+        syslog.syslog(msg)
         self.count = 0
         now = datetime.datetime.now()
-        seconds_left = (24 * 60 - (now.hour * 60 + now.minute)) * 60
-        timer = Timer(interval=seconds_left+1, function=self.report)
+#        seconds_left = (24 * 60 - (now.hour * 60 + now.minute)) * 60
+        seconds_left = 60
+        timer = Timer(interval=seconds_left, function=self.report)
         print(f'Sleeping for {seconds_left} seconds.', flush=True)
         timer.daemon = True
         timer.start()
 
     def start(self):
-        self.request_mail_credentials()
         self.report()
         with keyboard.Listener(on_release=self.handle_key_press) as listener:
             listener.join()
